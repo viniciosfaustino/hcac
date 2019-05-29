d22b0b4836abb2f893b3e8708ce36f9295d9b25a
viniciosfaustino <vinicioskf@gmail.com>
2019-05-29 12:01:37 -0400

    implementando funcoes de agrupamento

--- cluster_module.py
+++ cluster_module.py
@@ -1,9 +1,14 @@
+import numpy as np
 from dataset_module import Dataset
+
 
 class Cluster():
     def __init__(self, _number_of_elements:int):
-        self.number_of_elements = _number_of_elements
         self.entries = []
+        self.cluster_size = np.ones(_number_of_elements).tolist()
+
+    def add_entry(self, index_a: int, index_b:int, distance: float, number_of_elements: int):
+        self.entries.append([index_a, index_b, distance, number_of_elements])
 
     def get_fscore(self, dataset:Dataset):
         if dataset.label is None:
@@ -13,3 +18,7 @@ class Cluster():
 
     def get_class_from_cluster(self):
         return int
+
+    def get_new_entry_size(self, index: tuple):
+        num = self.cluster_size[index[0]] + self.cluster_size[index[1]]
+        self.
--- hcac_module.py
+++ hcac_module.py
@@ -20,10 +20,11 @@ class HCAC():
         self.cluster = Cluster(self.dataset.size)
 
         self.confidence_array = []
-        self.confidence_array = self.get_confidence()
+        self.confidence_array = self.get_confidence_array()
         self.threshold = self.get_threshold()
         self.distance_matrix = None
         self.set_distance_matrix()
+
 
     def set_distance_matrix(self):
         if self.distance_function == 'cosine':
@@ -34,18 +35,11 @@ class HCAC():
         np.fill_diagonal(distance, np.inf)
         self.distance_matrix = distance
 
-    def get_confidence(self) -> list[float]:
+    def get_confidence_array(self) -> list[float]:
         cluster = linkage(self.distance_matrix, method=self.linkage_method, metric=self.distance_function)
         merge_distances = cluster[:, 2]
         confidence = sorted(merge_distances)
         return confidence
-
-    def update_distance_matrix(self, index_a: int, index_b: int):
-        for i in range(self.distance_matrix.shape[0]):
-            self.distance_matrix[i][index_a] = (self.distance_matrix[i][index_a] + self.distance_matrix[i][index_b])/2.0
-            self.distance_matrix[index_a][i] = self.distance_matrix[i][index_a]
-        self.distance_matrix = np.delete(self.distance_matrix, index_b, axis=0)
-        self.distance_matrix = np.delete(self.distance_matrix, index_b, axis=1)
 
     def get_threshold(self) -> float:
         if self.max_user_intervention > 2:
@@ -54,8 +48,38 @@ class HCAC():
             return self.confidence_array[0]
 
     def do_clustering(self):
+        while self.distance_matrix.shape[0] > 2:
+            index_merge = self.get_pair_to_merge()
+            number_of_elements = self.cluster.get_entry_size(index_merge)
+            self.cluster.add_entry(index_merge[0], index_merge[1], self.distance_matrix[index_merge], 0)
         pass
 
+
+    def get_pair_to_merge(self) -> tuple:
+        min_dist_index = np.argmin(self.distance_matrix)
+        min_dist_index = np.unravel_index(min_dist_index, self.distance_matrix.shape)
+
+        if self.get_merge_confidence(min_dist_index) < self.threshold:
+            pass
+        else:
+            pass
+        return min_dist_index
+
+    def get_merge_confidence(self, index: tuple) -> float:
+        min_dist = self.distance_matrix[index]
+        aux_vec = np.delete(self.distance_matrix[index[0]], index[0])
+        aux_dist = [np.amin(aux_vec)]
+        aux_vec = np.delete(self.distance_matrix[index[1]], index[1])
+        aux_dist.append(np.amin(aux_vec))
+        return min(aux_vec) - min_dist
+
+
+    def update_distance_matrix(self, index_a: int, index_b: int):
+        for i in range(self.distance_matrix.shape[0]):
+            self.distance_matrix[i][index_a] = (self.distance_matrix[i][index_a] + self.distance_matrix[i][index_b])/2.0
+            self.distance_matrix[index_a][i] = self.distance_matrix[i][index_a]
+        self.distance_matrix = np.delete(self.distance_matrix, index_b, axis=0)
+        self.distance_matrix = np.delete(self.distance_matrix, index_b, axis=1)
 
     def create_pool(self, index:int):
         return list
