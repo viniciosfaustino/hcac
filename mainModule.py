d7ada504d4d700fd54a7e6acfe5172daa0eaf202
viniciosfaustino <vinicioskf@gmail.com>
2019-09-09 18:31:42 -0400

    adicionando datasets e mudando script de execução

--- mainModule.py
+++ mainModule.py
@@ -9,49 +9,51 @@ from mlModule import ML
 from datasetModule import Dataset
 
 from evaluationModule import get_fscore
-from utils import *
+from utils import split_data_label
 
 if __name__ == '__main__':
     np.set_printoptions(suppress=True)
-    datasets = ["new_iris", "new_wine2", "new_ecoli2", "new_ionosphere", "new_haberman", "new_segmentation2"]
+    # datasets = ["new_iris", "new_wine2", "new_ecoli2", "new_ionosphere", "new_haberman", "new_segmentation2"]
     # datasets = ["new_wine2", "new_ecoli2", "new_ionosphere", "new_haberman", "new_segmentation2"]
+
+    datasets = ["dilma.data", "brasui.data", "eleicao.data", "tweets.data", "solo.data"]
 
     user_interventions = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.]
     slack = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.]
-    # fh = open("datasets/bow_no_stopwords_eleicao.data", "rb")
-    # data, label = split_data_label(pickle.load(fh))
-    dataset = np.loadtxt("datasets/bow_no_stopwords_eleicao.data")
-    data, label = split_data_label(dataset)
-    dataset = Dataset(data, "bow_eleicao", label)
+
     pool_size = 5
-    print(dataset.name)
-    if not os.path.exists(os.path.join("results", dataset.name)):
-        os.mkdir(os.path.join("results", dataset.name))
-    for s in slack:
-        score_h = []
-        score_m = []
-        print("slack: ", s)
-        for ui in user_interventions:
-            print("intervention: ", ui, end="\r")
+    for d in datasets:
+        with open(os.path.join("datasets",d), "rb") as file_handler:
+            dataset = pickle.load(file_handler)
+        data, labels = split_data_label(dataset)
+        dataset = Dataset(data, d, _label=labels)
+        if not os.path.exists(os.path.join("results", dataset.name)):
+            os.mkdir(os.path.join("results", dataset.name))
+        for s in slack:
+            score_h = []
+            score_m = []
+            print("slack: ", s)
+            for ui in user_interventions:
+                print("intervention: ", ui, end="\r")
 
-            ml = ML(dataset, pool_size, int(ui * dataset.size) - 2, s)
-            ml.do_clustering()
-            score_m.append(get_fscore(ml))
-            hcac = HCAC(dataset, pool_size, int(ui * dataset.size) - 2)
-            hcac.do_clustering()
-            score_h.append(get_fscore(hcac))
+                ml = ML(dataset, pool_size, int(ui * dataset.size) - 2, s)
+                ml.do_clustering()
+                score_m.append(get_fscore(ml))
+                hcac = HCAC(dataset, pool_size, int(ui * dataset.size) - 2)
+                hcac.do_clustering()
+                score_h.append(get_fscore(hcac))
 
-        print()
+            print()
 
-        plt.plot(user_interventions, score_h, label="hcac", color="r")
-        plt.plot(user_interventions, score_m, label="ml", color="b")
-        plt.xlabel("User Intervention")
-        plt.ylabel("FScore")
-        plt.title(dataset.name + ": FScore x User intervention")
-        plt.legend()
-        path = os.path.join("results", dataset.name, dataset.name + "_" + str(s) + ".png")
-        plt.savefig(path)
-        plt.clf()
+            plt.plot(user_interventions, score_h, label="hcac", color="r")
+            plt.plot(user_interventions, score_m, label="ml", color="b")
+            plt.xlabel("User Intervention")
+            plt.ylabel("FScore")
+            plt.title(dataset.name + ": FScore x User intervention")
+            plt.legend()
+            path = os.path.join("results", dataset.name, dataset.name + "_" + str(s) + ".png")
+            plt.savefig(path)
+            plt.clf()
 
 
 
